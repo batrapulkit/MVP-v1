e7dad77aae7e2bfdfd05dfeb1ad9047e6d0e1750
Pulkit Batra <67047845+batrapulkit@users.noreply.github.com>
2025-11-13 15:59:04 -0500

    Wrap App component with HelmetProvider and StrictMode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,15 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const helmetContext = {};
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HelmetProvider context={helmetContext}>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+);
